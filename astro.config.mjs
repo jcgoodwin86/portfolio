197e418d9c80c3009059c84195d99454a1929545
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-08-02 13:51:27 -0400

    Update packages and settings for unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // astro.config.ts
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import presetUno from "@unocss/preset-uno";
 
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true, // or a path to the reset file
-      presets: [presetUno()],
     }),
   ],
 });
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,38 @@
 // uno.config.ts
-import { defineConfig } from "unocss";
-import presetUno from "@unocss/preset-uno";
+import { defineConfig, presetTypography, presetWebFonts, presetUno } from "unocss";
 
 export default defineConfig({
-  presets: [presetUno()],
+  presets: [
+  presetUno(),
+  presetTypography({
+    cssExtend: {
+      "h1, h2, h3, h4, h5, h6": {
+        "font-family": "Lekton",
+      },
+    },
+  }),
+  presetWebFonts({
+    provider: "google", // default provider
+    fonts: {
+      // these will extend the default theme
+      sans: "Roboto",
+      mono: ["Fira Code", "Fira Mono:400,700"],
+      // custom ones
+      lobster: "Lobster",
+      lekton: "Lekton",
+      spacemono: "Space Mono",
+      lato: [
+        {
+          name: "Lato",
+          weights: ["400", "700"],
+          italic: true,
+        },
+        {
+          name: "sans-serif",
+          provider: "none",
+        },
+      ],
+    },
+  }),
+]
 });
